6d2cfeb8cc5455a9cfed473fc3e0cad631ee3a48
Inkognito <inkognito_1987@list.ru>
2019-04-13 18:45:34 +0900

    Реализован связный список

--- src/Four/AppList.java
+++ src/Four/AppList.java
@@ -1,4 +1,15 @@
 package Four;
 
 public class AppList {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.inseert("One", 22);
+        list.inseert("Two", 27);
+        list.inseert("Three", 18);
+
+        list.display();
+        System.out.println();
+        if (!list.isEmpty()) list.delete();
+        list.display();
+    }
 }
--- src/Four/Link.java
+++ src/Four/Link.java
@@ -1,4 +1,16 @@
 package Four;
 
 public class Link {
+    public String name;
+    public int age;
+    public Link next;
+
+    public Link(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayNode(){
+        System.out.println("Name " + name + "; Age " + age);
+    }
 }
