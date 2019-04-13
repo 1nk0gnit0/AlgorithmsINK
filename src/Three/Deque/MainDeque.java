6d9c38a985e18762dd265dcca7a741798adfceb5
Inkognito <inkognito_1987@list.ru>
2019-04-13 21:58:37 +0900

    Исправил дек

--- src/Three/Deque/MainDeque.java
+++ src/Three/Deque/MainDeque.java
@@ -4,23 +4,16 @@ public class MainDeque {
     public static void main(String[] args) {
         Deque d = new Deque(5);
 
+        d.insertRight(1);
+        d.insertRight(2);
         d.insertRight(3);
 
-        d.insertLeft(2);
-
-        d.removeRight();
-
-        d.insertRight(4);
-
-        d.insertLeft(1);
-
-        d.removeRight();
-
-        d.insertRight(5);
-        d.insertRight(6);
-        d.insertRight(7);
+        d.insertLeft(4);
+        d.insertLeft(5);
 
         d.removeLeft();
+
+        d.removeRight();
 
         while (!d.isEmpty()){
             int n = d.removeLeft();
